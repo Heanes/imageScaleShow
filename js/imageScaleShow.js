a0fc6184378e5b078a0b60abd90a5b3278c2ce84
fanggang <fanggang@meicai.cn>
2016-08-14 02:55:34 +0800

    优化dom及样式

--- js/imageScaleShow.js
+++ js/imageScaleShow.js
@@ -7,11 +7,52 @@
 !(function ($, window, undefined) {
     $.fn.imageScaleShow = function (userConf) {
         var defaultConf = {
-                // 大图资源
+                // 小图资源
+                'imageSmallSrcList': [],
+                // 中图资源
+                'imageMiddleSrcList': [],
+                // 大图资源，若不填则直接将中图样式上拉伸放大显示
                 'imageBigSrcList': []
             },
             conf = $.extend({}, defaultConf, userConf || {});
         var $imageScaleShow = this;
+
+        // js指定所有图片形式
+        $imageScaleShow.empty();
+
+        // 中图的dom组织
+        var imageMiddleDomStr =
+            '<div class="image-show">'
+                + '<ul class="show-list"></ul>'
+                + '<div class="cursor-select-area" id="cursorSelectArea"></div>'
+            + '</div>';
+        var $imageMiddleDom = $(imageMiddleDomStr);
+        var $imageMiddleUlDom = $imageMiddleDom.find('.show-list');
+        $.each(conf.imageMiddleSrcList, function (i, item) {
+            var liClassStr = '';
+            if(i == 0){liClassStr = ' class="show"'}
+            $imageMiddleUlDom.append($('<li' + liClassStr + '><img src="' + item + '"/></li>'));
+        });
+        $imageScaleShow.append($imageMiddleDom);
+
+        // 小图的dom组织
+        var imageSmallDomStr =
+            '<div class="image-thumb">'
+                + '<a class="thumb-nav left" id="thumbNavLeft"><</a>'
+                + '<ul class="thumb-list"></ul>'
+                + '<a class="thumb-nav right" id="thumbNavRight">></a>'
+            + '</div>';
+        var $imageSmallDom = $(imageSmallDomStr);
+        var $imageSmallUlDom = $imageSmallDom.find('.thumb-list');
+        $.each(conf.imageSmallSrcList, function (i, item) {
+            var selectedClass = '';
+            if(i == 0){selectedClass = ' class="selected"'}
+            $imageSmallUlDom.append($('<li><a href="javascript:;"' + selectedClass + '><img src="' + item + '"/></a></li>'));
+        });
+        $imageScaleShow.append($imageSmallDom);
+        // 大图dom
+        $imageScaleShow.append($('<div class="image-show-big"></div>'));
+
         var $imageThumbList = $imageScaleShow.find('.image-thumb .thumb-list a');
         var $imageShowList = $imageScaleShow.find('.image-show .show-list li');
         var $imageShowBig = $imageScaleShow.find('.image-show-big');
@@ -29,13 +70,25 @@
         // 缩略图点击切换
         var selectImage = 0;
         $imageThumbList.each(function(i, item){
-            $(item).on('click mouseenter', function(){
+            $(item).on('click', function(){
                 $imageThumbList.removeClass('selected');
                 $(this).addClass('selected');
                 $imageShowList.removeClass('show');
                 $imageShowList.eq(i).addClass('show');
                 selectImage = i;
                 $imageShowBigImg.attr('src', imageBigSrcList[i]);
+            });
+            // 点击后鼠标移动到其他缩略图上只切换显示但鼠标移开后恢复原点击处
+            $(item).on('mouseenter', function(){
+                if($(this).hasClass('selected')) return;
+                $imageThumbList.not('.selected').removeClass('selected');
+                $imageShowList.removeClass('show');
+                $imageShowList.eq(i).toggleClass('show');
+                $imageShowBigImg.attr('src', imageBigSrcList[i]);
+            });
+            $(item).on('mouseleave', function(){
+                if($(this).hasClass('selected')) return;
+                $imageThumbList.eq(selectImage).trigger('click');
             });
         });
         // 缩略图导航左右切换
@@ -98,7 +151,7 @@
                     'top': moveTop
                 });
                 $cursorSelectArea.show();
-                // 放大的图片移动位子
+                // 放大的图片移动位置
                 $imageShowBigImg.css({
                     'left': -moveLeft * scaleRatio,
                     'top': -moveTop * scaleRatio
